d3e9cceba67d6a7427821409896c9850aa3efe8f
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-04-22 14:40:12 +0200

    add: milestone 1

--- js/script.js
+++ js/script.js
@@ -4,7 +4,38 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            contacts: [
+                {
+                    num: 1,
+                    name: "Michele",
+                    description: "Avatar Michele",
+                },
+                {
+                    num: 2,
+                    name: "Fabio",
+                    description: "Avatar Fabio",
+                },
+                {
+                    num: 3,
+                    name: "Samuele",
+                    description: "Avatar Samuele",
+                },
+                {
+                    num: 6,
+                    name: "Claudia",
+                    description: "Avatar Claudia",
+                },
+                {
+                    num: 7,
+                    name: "Federico",
+                    description: "Avatar Federico",
+                },
+                {
+                    num: 8,
+                    name: "Davide",
+                    description: "Avatar Davide",
+                },
+            ]
         }
     },
     methods: {
